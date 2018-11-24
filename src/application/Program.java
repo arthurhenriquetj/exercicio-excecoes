cafe9a3a640db3e34b19d6491c0ffbf430816a39
arthurhenriquetj <arthurhenriquetj@gmail.com>
2018-11-24 13:44:55 -0200

    programa implementado sem tratamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,40 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("ENTER ACCOUNT DATA");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial Balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw Limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account account = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.println("Enter amount to withdraw: ");
+		double amount = sc.nextDouble();
+		account.withDraw(amount);
+		System.out.println("New Balance: " + account.getBalance());
+		
 
+		
+		
+		
+		sc.close();
 	}
 
 }
